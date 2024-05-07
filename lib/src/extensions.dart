81e6a4de2c40f2044c4013ff031326c4aa780a90
Mohammad Rasim <mohammad.rasim96@gmail.com>
2024-05-07 20:59:51 +0300

    the toHexString can work with any iterable

--- lib/src/extensions.dart
+++ lib/src/extensions.dart
@@ -1,12 +1,10 @@
-import 'dart:typed_data';
-
 extension IntExtension on int {
   String toHexString() {
     return toRadixString(16).padLeft(2, '0');
   }
 }
 
-extension Uint8ListExtension on Uint8List {
+extension IterableExtensions on Iterable<int> {
   String toHexString({String empty = '', String separator = ''}) {
     return isEmpty ? empty : map((e) => e.toHexString()).join(separator);
   }
